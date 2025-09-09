283f0003b3ac0f30068060f1c4eec09b6b1d1997
syaifuddinzuhri <mochammadsyaifuddinz@gmail.com>
2025-09-09 18:27:22 +0700

    update opening page

--- src/components/GuestBox.tsx
+++ src/components/GuestBox.tsx
@@ -1,21 +1,21 @@
-import { Guest } from '@/types/sheet'
-import React from 'react'
+import { Guest } from "@/types/sheet";
+import React from "react";
 
 interface GuestBoxProps {
-    guest: Guest
+  guest: Guest;
 }
 
 const GuestBox = ({ guest }: GuestBoxProps) => {
-    return (
-        <div className="absolute bottom-20 left-8 right-8">
-            <p className='text-center mb-2 text-xs'>Kepada Yth.</p>
-            <div className='border border-yellow-300 rounded-lg py-4 px-6 text-center bg-white'>
-                <h5 className='font-bold'>{guest?.name}</h5>
-                <div className='h-[20]'></div>
-                <p>{guest?.address}</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="w-full mb-6 px-6">
+      <p className="text-center mb-2 text-xs">Kepada Yth.</p>
+      <div className="border border-yellow-500 rounded-lg py-4 px-6 text-center border-3 bg-box">
+        <h5 className="font-bold">{guest?.name}</h5>
+        <div className="h-[20]"></div>
+        <p>{guest?.address}</p>
+      </div>
+    </div>
+  );
+};
 
-export default GuestBox
+export default GuestBox;
--- src/pages/OpeningPage.tsx
+++ src/pages/OpeningPage.tsx
@@ -1,38 +1,56 @@
-'use client'
+"use client";
 
-import GuestBox from '@/components/GuestBox';
-import LoadingPage from '@/components/LoadingPage';
-import { useSheet } from '@/contexts/SheetContext'
-import { useSearchParams } from 'next/navigation';
-import React from 'react'
+import GuestBox from "@/components/GuestBox";
+import LoadingPage from "@/components/LoadingPage";
+import { useSheet } from "@/contexts/SheetContext";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 const OpeningPage = () => {
-    const { data } = useSheet()
+  const { data } = useSheet();
 
-    const searchParams = useSearchParams();
-    const guest = searchParams?.get("guest");
+  const searchParams = useSearchParams();
+  const guest = searchParams?.get("guest");
 
-    const guestData = data.find(
-        (g) => g.name.toLowerCase() === guest?.toLowerCase()
-    );
+  const guestData = data.find(
+    (g) => g.name.toLowerCase() === guest?.toLowerCase()
+  );
 
-    if (!data || data.length === 0) {
-        return <LoadingPage />
-    }
+  if (!data || data.length === 0) {
+    return <LoadingPage />;
+  }
 
-    return (
-        <div className='min-h-screen relative bg-yellow-50'>
-            <h1>Hello</h1>
-            <h1 className="font-cookie text-4xl">Wedding Invitation</h1>
-            <h1 className="font-imperial text-4xl">Save the Date</h1>
-            <h1 className="font-lobster text-4xl">Roikhatul Jannah</h1>
-            <h1 className="font-lobster text-4xl">Muhammad Solihin</h1>
-
-            {
-                guestData && <GuestBox guest={guestData} />
-            }
+  return (
+    <div className="min-h-screen relative bg-white section opening-page">
+      <div className="opening-content">
+        <img src={"/images/motif-3.webp"} className="motif-1" alt="" />
+        <div className="section-content mt-8">
+          <img src={"/images/motif-9.svg"} className="" alt="" />
+          <h1 className="font-cookie text-5xl text-center main-color">
+            Wedding Invitation
+          </h1>
+          <div className="flex flex-col items-center">
+            <img
+              src={"/images/motif-4.png"}
+              className="motif-2 mt-10 mb-10"
+              alt=""
+            />
+            <h1 className="font-lobster text-md text-center main-color">
+              Jum'at, 26 September 2025
+            </h1>
+          </div>
+          {guestData && <GuestBox guest={guestData} />}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default OpeningPage
+export default OpeningPage;
+
+{
+  /* <h1 className="font-cookie text-4xl">Wedding Invitation</h1>
+      <h1 className="font-imperial text-4xl">Save the Date</h1>
+      <h1 className="font-lobster text-4xl">Roikhatul Jannah</h1>
+      <h1 className="font-lobster text-4xl">Muhammad Solihin</h1> */
+}
