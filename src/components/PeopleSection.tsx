dedfb63baf0fff749106fafa9b18a48e5e92c383
syaifuddinzuhri <mochammadsyaifuddinz@gmail.com>
2025-09-10 17:21:46 +0700

    update

--- src/components/PeopleSection.tsx
+++ src/components/PeopleSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PeopleSection = () => {
   return (
-    <div className="h-screen relative overflow-hidden bg-color-2 p-5 flex flex-col justify-center items-center gap-5 people-section">
+    <div className="min-h-screen relative overflow-hidden bg-color-2 p-5 flex flex-col justify-center items-center gap-5 people-section">
       <div className="relative h-full w-full bg-color rounded-xl relative p-8 bg-white flex flex-col justify-around items-center text-center">
         <div className="flex flex-col items-center gap-5">
           <img
@@ -22,18 +22,28 @@ const PeopleSection = () => {
           </p>
         </div>
 
-        <div>
-          <p
+        <div className="pt-10">
+          {/* <p
             data-aos="zoom-in"
             data-aos-delay="100"
             className="font-pacifico main-color text-3xl font-medium mb-4"
           >
             Rikha
-          </p>
+          </p> */}
+          <div className="overflow-hidden rounded-full w-40 h-60 border-3 border-yellow-700 mx-auto mb-4 shadow-lg"
+           data-aos="zoom-in"
+            data-aos-delay="100">
+            <img
+              src="/images/female.png"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+
           <p
             data-aos="zoom-in"
             data-aos-delay="100"
-            className="font-roboto-slab main-color text-xl font-medium"
+            className="font-pacifico main-color text-2xl font-medium"
           >
             Roikhatul Jannah
           </p>
@@ -55,24 +65,36 @@ const PeopleSection = () => {
 
         <img
           src="/images/and.png"
-          width={80}
+          width={40}
+          className="my-10"
           alt=""
           data-aos="zoom-in"
           data-aos-delay="300"
         />
 
-        <div>
-          <p
+        <div className="pb-10">
+          {/* <p
             data-aos="zoom-in"
             data-aos-delay="400"
             className="font-pacifico main-color text-3xl font-medium mb-4"
           >
             Solihin
-          </p>
+          </p> */}
+          <div
+            className="overflow-hidden rounded-full w-40 h-60 border-3 border-yellow-700 mx-auto mb-4 shadow-lg"
+            data-aos="zoom-in"
+            data-aos-delay="400"
+          >
+            <img
+              src="/images/male.png"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
           <p
             data-aos="zoom-in"
             data-aos-delay="400"
-            className="font-roboto-slab main-color text-xl font-medium"
+            className="font-pacifico main-color text-2xl font-medium"
           >
             Muhammad Solihin
           </p>
@@ -91,6 +113,7 @@ const PeopleSection = () => {
             Karang Panas, Dayurejo, Prigen
           </p>
         </div>
+
         <div data-aos="fade-right" className="absolute bottom-0 left-0 w-[20%]">
           <img
             src="/images/motif-12.svg"
