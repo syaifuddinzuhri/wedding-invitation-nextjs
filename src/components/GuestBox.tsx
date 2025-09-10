9d7921f59f4762f5d300939210a4b9d02430c511
syaifuddinzuhri <mochammadsyaifuddinz@gmail.com>
2025-09-10 14:01:32 +0700

    update

--- src/components/GuestBox.tsx
+++ src/components/GuestBox.tsx
@@ -1,18 +1,33 @@
+"use client";
+import { useSheet } from "@/contexts/SheetContext";
 import { Guest } from "@/types/sheet";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
-interface GuestBoxProps {
-  guest: Guest;
-}
+const GuestBox = () => {
+  const { data } = useSheet();
 
-const GuestBox = ({ guest }: GuestBoxProps) => {
+  const searchParams = useSearchParams();
+  const guest = searchParams?.get("guest");
+
+  const guestData = data.find(
+    (g) => g.name.toLowerCase() === guest?.toLowerCase()
+  );
+
+  if (!guestData) return null;
+
   return (
-    <div className="w-full mb-6 px-6">
+    <div
+      className="w-full mb-6 px-6"
+      data-aos="zoom-in"
+      data-aos-delay="500"
+      data-aos-once="true"
+    >
       <p className="text-center mb-2 text-xs">Kepada Yth.</p>
       <div className="border border-yellow-500 rounded-lg py-4 px-6 text-center border-3 bg-box">
-        <h5 className="font-bold">{guest?.name}</h5>
+        <h5 className="font-bold">{guestData?.name}</h5>
         <div className="h-[20]"></div>
-        <p>{guest?.address}</p>
+        <p>{guestData?.address}</p>
       </div>
     </div>
   );
--- src/pages/OpeningPage.tsx
+++ src/pages/OpeningPage.tsx
@@ -1,57 +1,84 @@
 "use client";
 
 import GuestBox from "@/components/GuestBox";
-import LoadingPage from "@/components/LoadingPage";
 import { useSheet } from "@/contexts/SheetContext";
+import gsap from "gsap";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 interface OpeningPageProps {
   openButton: () => void;
 }
 
 const OpeningPage = ({ openButton }: OpeningPageProps) => {
-  const { data } = useSheet();
+  const motifRef = useRef<HTMLImageElement | null>(null);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
 
-  const searchParams = useSearchParams();
-  const guest = searchParams?.get("guest");
-
-  const guestData = data.find(
-    (g) => g.name.toLowerCase() === guest?.toLowerCase()
-  );
-
-  if (!data || data.length === 0) {
-    return <></>;
-  }
-
-  if (!guestData) {
-    return <></>;
-  }
+  useEffect(() => {
+    if (motifRef.current && buttonRef.current) {
+      gsap.timeline().from(buttonRef.current, {
+        opacity: 0,
+        y: 200,
+        duration: 4,
+        ease: "power3.out",
+      });
+    }
+  }, []);
 
   return (
     <div className="min-h-screen relative bg-white section opening-page">
       <div className="opening-content">
-        <img src={"/images/motif-3.webp"} className="motif-1" alt="" />
+        <img
+          src="/images/motif-3.webp"
+          className="motif-1 absolute bottom-0"
+          ref={motifRef}
+        />
+
         <div className="section-content">
-          <img src={"/images/motif-9.svg"} className="" alt="" />
-          <h1 className="font-cookie text-5xl text-center main-color">
+          <img
+            src={"/images/motif-9.svg"}
+            className=""
+            alt=""
+            data-aos="fade-down"
+            data-aos-once="true"
+          />
+          <h1
+            className="font-cookie text-5xl text-center main-color"
+            data-aos="fade-down"
+            data-aos-delay="500"
+            data-aos-once="true"
+          >
             Wedding Invitation
           </h1>
-          <h1 className="font-lobster text-3xl text-center main-color">
+          <h1
+            className="font-lobster text-3xl text-center main-color"
+            data-aos="zoom-in"
+            data-aos-delay="800"
+            data-aos-once="true"
+          >
             Rikha & Solihin
           </h1>
           <div className="flex flex-col items-center">
             <img
+              data-aos="zoom-in"
+              data-aos-delay="1000"
+              data-aos-once="true"
               src={"/images/motif-4.png"}
               className="motif-2 mt-10 mb-10"
               alt=""
             />
-            <h1 className="font-lobster text-md text-center main-color">
+            <h1
+              className="font-lobster text-md text-center main-color"
+              data-aos="zoom-in"
+              data-aos-delay="1000"
+              data-aos-once="true"
+            >
               Jum'at, 26 September 2025
             </h1>
           </div>
-          {guestData && <GuestBox guest={guestData} />}
+          <GuestBox />
           <button
+            ref={buttonRef}
             onClick={openButton}
             className="cursor-pointer bg-yellow-700 hover:bg-yellow-800 hover:shadow-lg main-color-2 font-medium py-2 px-8 rounded-full transition"
           >
